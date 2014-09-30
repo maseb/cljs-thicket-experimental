5baae22fa0ccbb91449f6cb7575cd58be963be7b
Mason Browne <mason.browne@nebula.com>
2014-09-29 17:26:41 -0700

    React demo.

--- src/cljs/client/mb/client/main.cljs
+++ src/cljs/client/mb/client/main.cljs
@@ -1,6 +1,31 @@
 (ns mb.client.main
-  (:require [mb.core :as c]))
+  (:require-macros [cljs.core.async.macros :refer [go-loop]])
+  (:require [mb.core :as c]
+            [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [timeout <!]]))
 
 (enable-console-print!)
 
-(println "Hello world! " c.woo)
+(def app-state (atom {:text "Hello, world!"}))
+
+(defn -main []
+
+  (om/root
+    (fn [app owner]
+      (reify om/IRender
+        (render [_]
+          (dom/h1 nil (:text app)))))
+    app-state
+    {:target (. js/document (getElementById "chrome-root"))})
+
+  (go-loop [flag 1]
+           (let [isOdd (odd? flag)]
+           (swap! app-state assoc :text (if isOdd "Odd" "Even"))
+           (<! (timeout 250))
+           (recur (if isOdd 0 1))))
+
+  (println "Running"))
+
+(-main)
+
